2b472d35467aa8a7caf534afa65c30f74bf3cc61
riyas-mp <muhamedriyas2222@gmail.com>
2025-07-11 22:43:49 +0530

    basics

---
+++ main.py
@@ -0,0 +1,10 @@
+name ="nikhil not study"
+
+values=["cross road","sps","pathon"]
+# print(len(values))
+values[0]="Hello"
+# print(values)
+values=values + ["how are you",10]
+# print (values)
+values.append(input("enter a value"))
+print (values)
